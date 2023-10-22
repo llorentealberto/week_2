0177538f1f74b2e7e62935171e2e18a677447cc2
[llorentealberto] <[llorentealberto@hotmal.com]>
2023-10-22 17:56:11 +0200

    subo cambios en los ejercciios , terminado el reto While

--- Ejercicios de Repaso FdP/retos1/buclesWhile.ts
+++ Ejercicios de Repaso FdP/retos1/buclesWhile.ts
@@ -1,31 +1,64 @@
 
-
+// Realizar una función que te devuelva si existe un numero par en el array de números que
+// introduces como parámetro de entrada.
+// La cabecera de la función tendrá el siguiente aspecto: function hasEven(myNums)
 
 
 function hasEven(myNums){
-    let pares=[]
-    for( let i=0 ; i<myNums.length ; i++){
+    
+    let i=0 ;
 
+    let esPar=false ;
 
-        if(myNums[i]%2==0 ){
-            
-            console.log("este numero " + myNums[i] + "es par");
-            
+    while(i<myNums.length && !esPar){
+
+        if(myNums[i]%2 ==0){
+            esPar=true;
         }
+        i++
     }
 
-
+    return esPar;
+    
 
 
 }
 
 let arrPrueba=[1 ,2 ,3 ,4];
+const resultado = hasEven(arrPrueba);
+if (resultado) {
+    console.log("uno de los numeros del array es par ");
+  } else {
+    console.log("No hay suerte , prueba con otro ");
+  }
+
+//   Realizar una función que reciba un array de nombres y te devuelva verdadero si y solo si
+//   todos los nombres empiezan por M.
+//   La cabecera de la función tendrá el siguiente aspecto: function startWithM(myNames)
 
 
-function startWithMM(){
+function starWithM(myNames):boolean{
 
-    
+        let i=0;
 
+        let empiezaPorM =true;
+
+        while(i<starWithM.length && empiezaPorM){
+
+            if(!myNames[i].startsWith("m")){
+
+                empiezaPorM=false;
+                
+                
+            }
+            i++;
+        }
+        return empiezaPorM ;
 
 
 }
+
+let pruebaBool=["aario" , "maria"];
+
+console.log(starWithM(pruebaBool));
+
